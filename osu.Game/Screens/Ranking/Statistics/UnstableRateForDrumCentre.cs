988589a3393c81f7342aedc09dcfba2da912a7e9
Bartosz Sieprawski <b.sieprawski@op.pl>
2025-01-25 20:41:09 +0100

    Add separate drum URs to Statistics screen
    The calculated Unstable Rate for both `DrumCentre` and `DrumRim` are now
    displayed in the result screen along the default Unstable Rate for all
    hit events.

---
+++ osu.Game/Screens/Ranking/Statistics/UnstableRateForDrumCentre.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using osu.Game.Rulesets.Scoring;
+
+namespace osu.Game.Screens.Ranking.Statistics
+{
+    /// <summary>
+    /// Displays the unstable rate statistic for a given play.
+    /// </summary>
+    public partial class UnstableRateForDrumCentre : SimpleStatisticItem<double?>
+    {
+        /// <summary>
+        /// Creates and computes an <see cref="UnstableRate"/> statistic specifically for Drum Centre (Dons).
+        /// </summary>
+        /// <param name="hitEvents">Sequence of <see cref="HitEvent"/>s to calculate the unstable rate based on.</param>
+        public UnstableRateForDrumCentre(IReadOnlyList<HitEvent> hitEvents)
+            : base("Unstable Rate for Dons")
+        {
+            Value = hitEvents.CalculateUnstableRateForDrumCentre()?.Result;
+        }
+
+        protected override string DisplayValue(double? value) => value == null ? "(not available)" : value.Value.ToString(@"N2");
+    }
+}
