1287a0dbfd2e4fd98b6bf84b7d8c7d600f032c65
darkstoorm <b.sieprawski@op.pl>
2026-05-31 16:45:52 +0200

    Add `TypingBeatmap` for stats and conversion
    I just copied it from Taiko ruleset

---
+++ osu.Game.Rulesets.Typing/Beatmaps/TypingBeatmap.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using osu.Game.Beatmaps;
+using osu.Game.Localisation;
+using osu.Game.Rulesets.Typing.Objects;
+
+namespace osu.Game.Rulesets.Typing.Beatmaps
+{
+    public class TypingBeatmap : Beatmap<TypingHitObject>
+    {
+        public override IEnumerable<BeatmapStatistic> GetStatistics()
+        {
+            int hits = HitObjects.Count;
+
+            return new[]
+            {
+                new BeatmapStatistic
+                {
+                    Name = BeatmapStatisticStrings.Hits,
+                    CreateIcon = () => new BeatmapStatisticIcon(BeatmapStatisticsIconType.Circles),
+                    Content = hits.ToString(),
+                    BarDisplayLength = hits,
+                },
+            };
+        }
+    }
+}
--- osu.Game.Rulesets.Typing/Beatmaps/TypingBeatmapConverter.cs
+++ osu.Game.Rulesets.Typing/Beatmaps/TypingBeatmapConverter.cs
@@ -1,4 +1,4 @@
-// Copyright (c) ppy Pty Ltd <contact@ppy.sh>.Licensed under the MIT Licence.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
@@ -35,6 +35,8 @@ namespace osu.Game.Rulesets.Typing.Beatmaps
                 Letter = randomTypingAction(),
             };
         }
+
+        protected override Beatmap<TypingHitObject> CreateBeatmap() => new TypingBeatmap();
 
         /// <summary>
         /// Returns a random key before the words provider is implemented, which will change how letters are picked.
