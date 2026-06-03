c95f9786af069cdf243c908b023a274c8ad08136
darkstoorm <b.sieprawski@op.pl>
2026-06-04 00:52:46 +0200

    Fix `ConstantSpeed` being super slow
    I took the ComputeTimeRange() part from drawable taiko ruleset and picked some arbitrary values to not have to reimplement the whole playfield adjustment, because I only needed the scroll speed to behave as expected (somewhat).

--- osu.Game.Rulesets.Typing/UI/DrawableTypingRuleset.cs
+++ osu.Game.Rulesets.Typing/UI/DrawableTypingRuleset.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using osu.Framework.Allocation;
 using osu.Framework.Input;
 using osu.Game.Beatmaps;
+using osu.Game.Configuration;
 using osu.Game.Input.Handlers;
 using osu.Game.Replays;
 using osu.Game.Rulesets.Mods;
@@ -20,11 +21,26 @@ namespace osu.Game.Rulesets.Typing.UI
     [Cached]
     public partial class DrawableTypingRuleset : DrawableScrollingRuleset<TypingHitObject>
     {
+        // This value comes from an arbitrary multiplier chosen from min/max ratio (1.35)
+        // applied to the ComputeTimeRange() from taiko playfield adjustment container
+        // to have a quick, hacky solution to ModConstantSpeed working in this ruleset
+        private const double time_length_in_ms = 3500;
+
         public DrawableTypingRuleset(TypingRuleset ruleset, IBeatmap beatmap, IReadOnlyList<Mod>? mods = null)
             : base(ruleset, beatmap, mods)
         {
             Direction.Value = ScrollingDirection.Left;
-            TimeRange.Value = 6000;
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            double multiplier = VisualisationMethod == ScrollVisualisationMethod.Constant
+                ? Beatmap.BeatmapInfo.BPM * Beatmap.Difficulty.SliderMultiplier / 60
+                : 1;
+
+            TimeRange.Value = multiplier / time_length_in_ms;
         }
 
         protected override Playfield CreatePlayfield() => new TypingPlayfield();
