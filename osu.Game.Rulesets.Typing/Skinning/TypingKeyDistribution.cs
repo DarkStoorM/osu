17d70edbbf393267bdb5706843a087d12597adf9
darkstoorm <b.sieprawski@op.pl>
2026-07-05 17:42:28 +0200

    Add skinnable key timing distribution
    Allows placing the key timing distribution on a skin layer

---
+++ osu.Game.Rulesets.Typing/Skinning/TypingKeyDistribution.cs
@@ -0,0 +1,45 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Rulesets.Judgements;
+using osu.Game.Rulesets.Scoring;
+using osu.Game.Rulesets.Typing.Screens.Ranking.Statistics;
+using osu.Game.Skinning;
+
+namespace osu.Game.Rulesets.Typing.Skinning
+{
+    public partial class TypingKeyDistribution : Container, ISerialisableDrawable
+    {
+        private KeyTimingDistribution keyTimingDistribution = null!;
+
+        public bool UsesFixedAnchor { get; set; }
+
+        [Resolved]
+        private ScoreProcessor scoreProcessor { get; set; } = null!;
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            AutoSizeAxes = Axes.Both;
+
+            keyTimingDistribution = new KeyTimingDistribution(scoreProcessor.HitEvents, scoreProcessor.Mods.Value);
+
+            Child = keyTimingDistribution;
+            Child.Alpha = 0.3f;
+
+            scoreProcessor.NewJudgement += updateKeyCards;
+        }
+
+        private void updateKeyCards(JudgementResult result)
+        {
+            if (scoreProcessor.HitEvents.Count == 0)
+                return;
+
+            keyTimingDistribution.UpdateKeyCard(result, scoreProcessor.HitEvents);
+        }
+    }
+}
