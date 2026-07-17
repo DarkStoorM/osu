5ebb5630dd29d0f263f6a58675d98b84c3dcc5ba
Bartosz Sieprawski <b.sieprawski@op.pl>
2026-07-17 17:48:21 +0200

    Add approximate WPM
    This adds WPM for the mod customisation of the `TypingModWords`:
    
    - Half beat length
    - Full beat length
    - Double beat length

--- osu.Game.Rulesets.Typing/Beatmaps/TypingBeatmap.cs
+++ osu.Game.Rulesets.Typing/Beatmaps/TypingBeatmap.cs
@@ -11,17 +11,41 @@ namespace osu.Game.Rulesets.Typing.Beatmaps
     {
         public override IEnumerable<BeatmapStatistic> GetStatistics()
         {
-            int hits = HitObjects.Count;
+            // int hits = HitObjects.Count;
 
             return new[]
             {
-                new BeatmapStatistic
-                {
-                    Name = "Characters",
-                    CreateIcon = () => new BeatmapStatisticIcon(BeatmapStatisticsIconType.Circles),
-                    Content = hits.ToString(),
-                    BarDisplayLength = hits,
-                },
+                // Disabled until there is a way to get these stats on post-process (after mod application)
+                // new BeatmapStatistic
+                // {
+                //     Name = "Characters",
+                //     CreateIcon = () => new BeatmapStatisticIcon(BeatmapStatisticsIconType.Circles),
+                //     Content = hits.ToString(),
+                //     BarDisplayLength = hits,
+                // },
+                // Note: rate change has no effect on this
+                getWpmStatistic("HWPM", 2),
+                getWpmStatistic("FBPM", 1),
+                getWpmStatistic("DBPM", 0.5),
+            };
+        }
+
+        private BeatmapStatistic getWpmStatistic(string label, double modBeatDivisor)
+        {
+            double beatLength = GetMostCommonBeatLength();
+
+            // Standard typing WPM assumes 5 characters per word.
+            // Main mod inserts letters every half beat, so:
+            // - letterTime = beatLength / 2
+            // - CPM = 120000 / beatLength
+            // - WPM = CPM / 5 = 24000 / beatLength
+            double wpm = 24000.0 / (beatLength / modBeatDivisor);
+
+            return new BeatmapStatistic
+            {
+                Name = label,
+                CreateIcon = () => new BeatmapStatisticIcon(BeatmapStatisticsIconType.Circles),
+                Content = $"~{wpm:N0}",
             };
         }
     }
