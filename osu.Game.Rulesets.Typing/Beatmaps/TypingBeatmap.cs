2a695154481d15b52ae1ef9d4ffda70a3a23d946
darkstoorm <b.sieprawski@op.pl>
2026-07-18 00:12:08 +0200

    Remove `BeatmapStatistics`
    This was initially supposed to display the WPM, but since those are not
    using the processed beatmap data, I had to remove it altogether, because
    that information was useless anyway, along with the hit object count,
    which is not being displayed after the mod was applied.
    
    I moved the WPM to the displayed beatmap attribues, since it has access
    to selected mods and mod customisation is being reflected in there.

--- osu.Game.Rulesets.Typing/Beatmaps/TypingBeatmap.cs
+++ osu.Game.Rulesets.Typing/Beatmaps/TypingBeatmap.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Typing.Objects;
@@ -11,42 +12,7 @@ namespace osu.Game.Rulesets.Typing.Beatmaps
     {
         public override IEnumerable<BeatmapStatistic> GetStatistics()
         {
-            // int hits = HitObjects.Count;
-
-            return new[]
-            {
-                // Disabled until there is a way to get these stats on post-process (after mod application)
-                // new BeatmapStatistic
-                // {
-                //     Name = "Characters",
-                //     CreateIcon = () => new BeatmapStatisticIcon(BeatmapStatisticsIconType.Circles),
-                //     Content = hits.ToString(),
-                //     BarDisplayLength = hits,
-                // },
-                // Note: rate change has no effect on this
-                getWpmStatistic("HWPM", 2),
-                getWpmStatistic("FBPM", 1),
-                getWpmStatistic("DBPM", 0.5),
-            };
-        }
-
-        private BeatmapStatistic getWpmStatistic(string label, double modBeatDivisor)
-        {
-            double beatLength = GetMostCommonBeatLength();
-
-            // Standard typing WPM assumes 5 characters per word.
-            // Main mod inserts letters every half beat, so:
-            // - letterTime = beatLength / 2
-            // - CPM = 120000 / beatLength
-            // - WPM = CPM / 5 = 24000 / beatLength
-            double wpm = 24000.0 / (beatLength / modBeatDivisor);
-
-            return new BeatmapStatistic
-            {
-                Name = label,
-                CreateIcon = () => new BeatmapStatisticIcon(BeatmapStatisticsIconType.Circles),
-                Content = $"~{wpm:N0}",
-            };
+            return Array.Empty<BeatmapStatistic>();
         }
     }
 }
