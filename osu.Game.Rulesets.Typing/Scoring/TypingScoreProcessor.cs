7b03a11f9dbeac8d8481f3e857db426cf95a8692
darkstoorm <bsdarkstoorm@gmail.com>
2026-08-12 01:16:42 +0200

    Bump bonus score for Space to 25%
    Due to the difficulty of just using the spaces from mod customisation,
    the extra score you get should be pretty rewarding, because it's easy to
    mess things up.

--- osu.Game.Rulesets.Typing/Scoring/TypingScoreProcessor.cs
+++ osu.Game.Rulesets.Typing/Scoring/TypingScoreProcessor.cs
@@ -12,7 +12,7 @@ namespace osu.Game.Rulesets.Typing.Scoring
 {
     public partial class TypingScoreProcessor : ScoreProcessor
     {
-        private const double bonus_space_score_fraction = 0.1;
+        private const double bonus_space_score_fraction = 0.25;
         private const double bonus_space_score_budget = MAX_SCORE * bonus_space_score_fraction;
 
         private double perSpaceBonus;
