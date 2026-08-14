7bdf1ba841a33ad9ddda092373a444d39173cbe2
darkstoorm <bsdarkstoorm@gmail.com>
2026-08-14 22:38:44 +0200

    Use `OsuColour` for coloured letters
    I picked the closest colours to what I had initially. Since osu! already
    had colours defined like that, why not just use them.
    
    The colours themselves are somewhat balanced for my personal taste. They
    can't be any darker/brighter or saturated/desaturated.

--- osu.Game.Rulesets.Typing/Objects/Drawables/DrawableTypingHitObject.cs
+++ osu.Game.Rulesets.Typing/Objects/Drawables/DrawableTypingHitObject.cs
@@ -27,21 +27,12 @@ namespace osu.Game.Rulesets.Typing.Objects.Drawables
         // change to get this to work since I couldn't figure out how to do this directly inside the mod in
         // IApplicableToDrawableHitObject. HitObject is null at that point, so I moved this here. Not ideal, but this
         // will eventually be replaced with ruleset-specific settings
-        private readonly Dictionary<Finger, Colour4> fingerColours = new Dictionary<Finger, Colour4>
-        {
-            // Blue-ish
-            { Finger.Index, Color4Extensions.FromHex("#0dccf2") },
-            // Green-ish
-            { Finger.Middle, Color4Extensions.FromHex("#40bf40") },
-            // Orange-ish
-            { Finger.Ring, Color4Extensions.FromHex("#ffbf00") },
-            // Red-ish
-            { Finger.Pinky, Color4Extensions.FromHex("#e64c4c") },
-        };
+        private readonly Dictionary<Finger, Colour4> fingerColours = new Dictionary<Finger, Colour4>();
 
         private const float font_size = 100;
 
         private readonly Colour4 defaultLetterColour = Color4Extensions.FromHex("#CDD6F4");
+        private Colour4 spaceColour;
 
         private OsuSpriteText letterText = null!;
         private TypingAction? typingAction;
@@ -59,8 +50,15 @@ namespace osu.Game.Rulesets.Typing.Objects.Drawables
             : base(hitObject) { }
 
         [BackgroundDependencyLoader]
-        private void load()
+        private void load(OsuColour colours)
         {
+            fingerColours.Add(Finger.Index, colours.Blue1);
+            fingerColours.Add(Finger.Middle, colours.Green2);
+            fingerColours.Add(Finger.Ring, colours.YellowDark);
+            fingerColours.Add(Finger.Pinky, colours.Red1);
+
+            spaceColour = colours.Purple1;
+
             Origin = Anchor.Centre;
 
             letterText = new OsuSpriteText
@@ -84,8 +82,9 @@ namespace osu.Game.Rulesets.Typing.Objects.Drawables
                 // This is very specific only to the current object that represents a space, which is the Pipe|Character
                 // so it appears as a word separator vertically across the playfield. Change this accordingly if a
                 // different character is used
-                letterText.Colour = defaultLetterColour;
-                letterText.Alpha = 0.5f;
+                if (OverrideKeyColour)
+                    letterText.Colour = spaceColour;
+
                 letterText.Text = space_text_character;
                 letterText.Scale = new Vector2(2, 2);
             }
