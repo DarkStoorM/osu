7e138bc9d70fbc097e628181522b6ce89b591448
darkstoorm <bsdarkstoorm@gmail.com>
2026-08-15 23:34:12 +0200

    Revert `Spaces` bonus, rebalance OD bonus
    The spaces are hard to play anyway and they make it easier to miss, so,
    naturally, the bonus should remain higher.
    
    The OD bonus though, it required a change, because since the total score
    is capped at one million, OD 0 and OD 5 for example would yield
    different results, favouring OD 0, because it widens the hit window,
    making higher OD plays not worth it.
    
    I decided to set the max bonus to 25% and keep the mid point at OD 5, so
    at this point the total score is one million, below this the score will
    drop. Increasing the OD should reward the player no matter what.

--- osu.Game.Rulesets.Typing/Scoring/TypingScoreProcessor.cs
+++ osu.Game.Rulesets.Typing/Scoring/TypingScoreProcessor.cs
@@ -1,7 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.Linq;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Difficulty.Utils;
@@ -13,15 +12,15 @@ namespace osu.Game.Rulesets.Typing.Scoring
 {
     public partial class TypingScoreProcessor : ScoreProcessor
     {
-        private const double bonus_space_score_fraction = 0.15;
+        private const double bonus_space_score_fraction = 0.25;
         private const double bonus_space_score_budget = MAX_SCORE * bonus_space_score_fraction;
 
         /// <summary>
-        /// Will grant extra score above this value of Accuracy.
+        /// Overall Difficulty (or Accuracy) where the score multiplier is 1. Below this value, multiplier is negative, positive above it.
         /// </summary>
-        private const int min_overall_difficulty_for_bonus = 5;
+        private const float overall_difficulty_mid_point = 5;
 
-        private const float max_overall_difficulty_bonus = 0.10f;
+        private const float overall_difficulty_multiplier = 0.25f;
 
         private double perSpaceBonus;
         private double overallDifficultyMultiplier;
@@ -68,18 +67,19 @@ namespace osu.Game.Rulesets.Typing.Scoring
         }
 
         /// <summary>
-        /// Returns the multiplier to be applied to the total score, which at most can be <see cref="max_overall_difficulty_bonus"/>,
-        /// and is calculated based on the clamped Overall Difficulty of the beatmap, so the bonus is awarded only above some
-        /// set value.
+        /// Returns the multiplier to be applied to the total score, which at most can be <see cref="overall_difficulty_multiplier"/>.
+        /// <para/>This creates a range of score multipliers from <c>-multiplier to +multiplier</c>, resulting in an example
+        /// total score of <c>750000 up to 1250000</c>.
+        /// <para/>This is purely for balance purposes, because zero OD SS play could grant 1000000 score if played perfectly,
+        /// making it unfair against OD 10 plays, which could barely scratch 1000000 with accuracy drops.
+        /// <para/>Bonus value for OD was also increased for this reason.
         /// </summary>
         private double calculateOverallDifficultyMultiplier(float? overallDifficulty)
         {
             if (overallDifficulty == null)
                 return 1;
 
-            float odClamped = Math.Clamp((float)overallDifficulty, min_overall_difficulty_for_bonus, 10) - min_overall_difficulty_for_bonus;
-
-            return 1.0 + odClamped / min_overall_difficulty_for_bonus * max_overall_difficulty_bonus;
+            return 1.0 + ((float)overallDifficulty - overall_difficulty_mid_point) / overall_difficulty_mid_point * overall_difficulty_multiplier;
         }
 
         protected override double GetBonusScoreChange(JudgementResult result) => perSpaceBonus;
