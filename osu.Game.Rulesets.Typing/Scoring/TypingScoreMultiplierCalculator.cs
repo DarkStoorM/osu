0e35a419bae31d80f7e797f6df1d62498dd82c28
Bartosz Sieprawski <b.sieprawski@op.pl>
2026-08-18 19:16:45 +0200

    Add DT/HT mod multipliers

--- osu.Game.Rulesets.Typing/Scoring/TypingScoreMultiplierCalculator.cs
+++ osu.Game.Rulesets.Typing/Scoring/TypingScoreMultiplierCalculator.cs
@@ -11,6 +11,18 @@ namespace osu.Game.Rulesets.Typing.Scoring
         public TypingScoreMultiplierCalculator(ScoreMultiplierContext context)
             : base(context)
         {
+            // HalfTime should greatly decrease the multiplier only for Halved beat length, because that customisation
+            // option increases the mod multiplier. Both can't really coexist, because Halved beat length + HalfTime
+            // adjustment will give free score when maximum value is applied, resulting in decreasing the speed that
+            // matches Default beat length.
+            Combination<TypingModHalfTime, TypingModWords>(hasMultiplier: (halfTime, words) => words.AdjustBeatLength.Value == BeatLengthAdjustment.Halved
+                ? halfTime.SpeedChange.Value / 2
+                : halfTime.SpeedChange.Value);
+
+            // With DoubleTime, the HT issue is not that important since it would make no sense to apply DT + Doubled beat length
+            // to play with the same speed. The resulting multiplier will still be 1 on 2x DT, so it will only make the song fast
+            Single<TypingModDoubleTime>(hasMultiplier: doubleTime => doubleTime.SpeedChange.Value);
+
             // This is still dodgy anyway, because the BPM dictates everything. If the beatmap was mapped to 240bpm for example,
             // it would require the usage of default, defined full beat length, which gives the 1.0 multiplier. Then, given
             // how 120bpm beatmap would be identical with halved beat length and awarding twice the score is a bit confusing.
